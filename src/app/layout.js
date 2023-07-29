660a05bcd7ef4fb1285bae6649399b6e7f12afc1
Rowell Jay Rodriguez <row.rodriguez@awesomeos.com>
2023-07-29 22:11:27 +0800

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
+import Navbar from '@/components/nabvar/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <div className='container'>
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
+      </body>
     </html>
   )
 }
